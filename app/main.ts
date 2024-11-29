3d8158783c26dc8b03b792d48e95118743df4c1e
Sami M. <sami.mal@outlook.com>
2024-11-29 22:36:42 +0300

    PONG

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
 import * as net from "net";
-
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  connection.write("+PONG\r\n")
 });
 //
 server.listen(6379, "127.0.0.1");
