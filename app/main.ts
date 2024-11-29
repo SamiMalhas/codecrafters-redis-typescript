ee768c158a273b96c26a840c173d32d502d08ee1
Sami M. <sami.mal@outlook.com>
2024-11-29 23:08:11 +0300

    multiple PONGS

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,18 @@
 import * as net from "net";
-const server: net.Server = net.createServer((connection: net.Socket) => {
-  connection.write("+PONG\r\n")
-});
 
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.error("Logs from your program will appear here!");
+
+const server: net.Server = net.createServer((connection: net.Socket) => {
+  connection.on("data", (data) => {
+    console.log("data ", data);
+    connection.write("+PONG\r\n");
+    connection.end();
+  });
+  connection.on("close", () => {
+    connection.end();
+    server.close();
+  });
+});
 
 server.listen(6379, "127.0.0.1");
